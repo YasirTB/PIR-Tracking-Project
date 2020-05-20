a1f32c23f99f722cf28c1b8532a6094537571df8
Miguel Carlos Juan <48310121+miguelcajuan@users.noreply.github.com>
2020-05-20 17:11:54 +1200

    Update dataProcessing.py
    added bar chart

--- dataProcessing.py
+++ dataProcessing.py
@@ -121,6 +121,38 @@ def summary(sensors):
         totalFreq += i.frequency
     return totalFreq
 
+
+def testGraph(timeData, readData):
+    import tkinter as tk
+    from pandas import DataFrame
+    import matplotlib.pyplot as plt
+    from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+    dTime = {'Sensor': ['0', '1', '2', '3', '4', '5'], 'Duration': timeData}
+    dRead = {'Sensor': ['0', '1', '2', '3', '4', '5'], 'Count': readData}
+
+    timeDF = DataFrame(dTime, columns=['Sensor', 'Duration'])
+    readDF = DataFrame(dRead, columns=['Sensor', 'Count'])
+
+    root = tk.Tk()
+
+    figure1 = plt.Figure(figsize=(6, 5), dpi=100)
+    ax1 = figure1.add_subplot(111)
+    bar1 = FigureCanvasTkAgg(figure1, root)
+    bar1.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+    timeDF = timeDF[['Sensor', 'Duration']].groupby('Sensor').sum()
+    timeDF.plot(kind='bar', legend=True, ax=ax1)
+    ax1.set_title('Time spent by the user on each zone')
+
+    figure2 = plt.Figure(figsize=(6, 5), dpi=100)
+    ax2 = figure2.add_subplot(111)
+    bar2 = FigureCanvasTkAgg(figure2, root)
+    bar2.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+    readDF = readDF[['Sensor', 'Count']].groupby('Sensor').sum()
+    readDF.plot(kind='bar', legend=True, ax=ax2)
+    ax2.set_title('Sensor read')
+
+    root.mainloop()
 def main():
     preArray = preprocess("C:/Users/Miguel/OneDrive/AUT/Final Year Project/data/tommy_inout.txt")
     Sensors = initSensors()
@@ -131,15 +163,15 @@ def main():
     # for i in Sensors:
     #     print('Sensor',i.id,round((i.frequency/tFreq)*100,2), '%', i.elapsedTime,'sec',i.frequency,'times')
     g1,g2 = sensorStats(Sensors)
-    print(preArray, g1, g2)
+
+    testGraph(g1,g2)
 
     ##Filtered
-    filtSen = initSensors()#instantiates 6 "new" Sensors
-    filtData = filterData(preArray,10)#last 10 sec of the data
+    filtSen = initSensors()
+    filtData = filterData(preArray,10)
     #filtStats
     calculate(filtData,filtSen)
-    d1, d2 = sensorStats(filtSen)
-    print(filtData,d1,d2)
-
+    timeData, readData = sensorStats(filtSen)
+    testGraph(timeData,readData)
 
 main()
