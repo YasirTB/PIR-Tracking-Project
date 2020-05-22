c49a045be2caa42a208cdd0f7871f98ac4639b59
YasirTB <41215262+YasirTB@users.noreply.github.com>
2020-05-22 15:49:54 +1200

    Update PIRApp.py

--- PIRApp.py
+++ PIRApp.py
@@ -29,7 +29,6 @@ def addfile():
 
 
 def resetMap():
-    canvas.itemconfig(zn0, fill='white')
     canvas.itemconfig(zn1, fill='white')
     canvas.itemconfig(zn12, fill='white')
     canvas.itemconfig(zn2, fill='white')
@@ -41,35 +40,63 @@ def resetMap():
     canvas.itemconfig(zn5, fill='white')
     canvas.itemconfig(zn51, fill='white')
 
+    canvas.itemconfig(zn1I, fill='white')
+    canvas.itemconfig(zn12I, fill='white')
+    canvas.itemconfig(zn2I, fill='white')
+    canvas.itemconfig(zn23I, fill='white')
+    canvas.itemconfig(zn3I, fill='white')
+    canvas.itemconfig(zn34I, fill='white')
+    canvas.itemconfig(zn4I, fill='white')
+    canvas.itemconfig(zn45I, fill='white')
+    canvas.itemconfig(zn5I, fill='white')
+    canvas.itemconfig(zn51I, fill='white')
 
-def update(tkn):
+
+def update(tkn, colour):
     if len(tkn) == 1:
-        if '0' in tkn:
-            canvas.itemconfig(zn0, fill='red')
-        elif '1' in tkn:
-            canvas.itemconfig(zn1, fill='red')
+        if '1' in tkn:
+            canvas.itemconfig(zn1, outline='black', fill=colour)
         elif '2' in tkn:
-            canvas.itemconfig(zn2, fill='red')
+            canvas.itemconfig(zn2, outline='black', fill=colour)
         elif '3' in tkn:
-            canvas.itemconfig(zn3, fill='red')
+            canvas.itemconfig(zn3, outline='black', fill=colour)
         elif '4' in tkn:
-            canvas.itemconfig(zn4, fill='red')
+            canvas.itemconfig(zn4, outline='black', fill=colour)
         elif '5' in tkn:
-            canvas.itemconfig(zn5, fill='red')
-    else:
-        # run combination
-        if '0' in tkn:
-            canvas.itemconfig(zn0, fill='red')
-        elif '1' in tkn and '2' in tkn:
-            canvas.itemconfig(zn12, fill='red')
+            canvas.itemconfig(zn5, outline='black', fill=colour)
+    elif len(tkn) == 2:
+        if '1' in tkn and '2' in tkn:
+            canvas.itemconfig(zn12, outline='black', fill=colour)
         elif '2' in tkn and '3' in tkn:
-            canvas.itemconfig(zn23, fill='red')
+            canvas.itemconfig(zn23, outline='black', fill=colour)
         elif '3' in tkn and '4' in tkn:
-            canvas.itemconfig(zn34, fill='red')
+            canvas.itemconfig(zn34, outline='black', fill=colour)
         elif '4' in tkn and '5' in tkn:
-            canvas.itemconfig(zn45, fill='red')
+            canvas.itemconfig(zn45, outline='black', fill=colour)
         elif '5' in tkn and '1' in tkn:
-            canvas.itemconfig(zn51, fill='red')
+            canvas.itemconfig(zn51, outline='black', fill=colour)
+        elif '0' in tkn and '1' in tkn:
+            canvas.itemconfig(zn1I, outline='black', fill=colour)
+        elif '0' in tkn and '2' in tkn:
+            canvas.itemconfig(zn2I, outline='black', fill=colour)
+        elif '0' in tkn and '3' in tkn:
+            canvas.itemconfig(zn3I, outline='black', fill=colour)
+        elif '0' in tkn and '4' in tkn:
+            canvas.itemconfig(zn4I, outline='black', fill=colour)
+        elif '0' in tkn and '5' in tkn:
+            canvas.itemconfig(zn5I, outline='black', fill=colour)
+    else:
+        if '0' in tkn:
+            if '1' in tkn and '2' in tkn:
+                canvas.itemconfig(zn12I, outline='black', fill=colour)
+            elif '2' in tkn and '3' in tkn:
+                canvas.itemconfig(zn23I, outline='black', fill=colour)
+            elif '3' in tkn and '4' in tkn:
+                canvas.itemconfig(zn34I, outline='black', fill=colour)
+            elif '4' in tkn and '5' in tkn:
+                canvas.itemconfig(zn45I, outline='black', fill=colour)
+            elif '5' in tkn and '1' in tkn:
+                canvas.itemconfig(zn51I, outline='black', fill=colour)
 
 
 index = 0
@@ -125,18 +152,52 @@ cWidth = 800
 cHeight = 650
 canvas = Canvas(tabFrame1, width=cWidth, height=cHeight)
 
-zn23 = canvas.create_polygon(285, 303, 325, 274, 276, 122, 156, 209, outline='blue', fill='white', tag='z2_3')
-zn2 = canvas.create_polygon(110, 350, 270, 350, 285, 303, 156, 209, outline='black', fill='white', tag='z2')
-zn12 = canvas.create_polygon(285, 397, 270, 350, 110, 350, 156, 491, outline='purple', fill='white', tag='z1_2')
+# Outer Zones
 zn1 = canvas.create_polygon(276, 578, 325, 426, 285, 397, 156, 491, outline='black', fill='white', tag='z1')
-zn51 = canvas.create_polygon(424, 578, 375, 426, 325, 426, 276, 578, outline='orange', fill='white', tag='z5_1')
-zn5 = canvas.create_polygon(544, 491, 415, 397, 375, 426, 424, 578, outline='black', fill='white', tag='z5')
-zn45 = canvas.create_polygon(590, 350, 430, 350, 415, 397, 544, 491, outline='cyan', fill='white', tag='z4_5')
-zn4 = canvas.create_polygon(544, 209, 415, 303, 430, 350, 590, 350, outline='black', fill='white', tag='z4')
-zn34 = canvas.create_polygon(424, 122, 375, 274, 415, 303, 544, 209, outline='green', fill='white', tag='z3_4')
+zn12 = canvas.create_polygon(285, 397, 270, 350, 110, 350, 156, 491, outline='black', fill='white', tag='z1_2')
+zn2 = canvas.create_polygon(110, 350, 270, 350, 285, 303, 156, 209, outline='black', fill='white', tag='z2')
+zn23 = canvas.create_polygon(285, 303, 325, 274, 276, 122, 156, 209, outline='black', fill='white', tag='z2_3', )
 zn3 = canvas.create_polygon(424, 122, 276, 122, 325, 274, 375, 274, outline='black', fill='white', tag='z3')
-zn0 = canvas.create_polygon(375, 274, 325, 274, 285, 303, 270, 350, 285, 397, 325, 426, 375, 426, 415, 397, 430, 350,
-                            415, 303, outline='black', fill='white', tag='z0')
+zn34 = canvas.create_polygon(424, 122, 375, 274, 415, 303, 544, 209, outline='black', fill='white', tag='z3_4')
+zn4 = canvas.create_polygon(544, 209, 415, 303, 430, 350, 590, 350, outline='black', fill='white', tag='z4')
+zn45 = canvas.create_polygon(590, 350, 430, 350, 415, 397, 544, 491, outline='black', fill='white', tag='z4_5')
+zn5 = canvas.create_polygon(544, 491, 415, 397, 375, 426, 424, 578, outline='black', fill='white', tag='z5')
+zn51 = canvas.create_polygon(424, 578, 375, 426, 325, 426, 276, 578, outline='black', fill='white', tag='z5_1')
+
+# Inner Zones
+zn1I = canvas.create_polygon(325, 426, 285, 397, 350, 350, outline='black', fill='white', tag='z1I')
+zn12I = canvas.create_polygon(285, 397, 270, 350, 350, 350, outline='black', fill='white', tag='z1_2I')
+zn2I = canvas.create_polygon(270, 350, 285, 303, 350, 350, outline='black', fill='white', tag='z2I')
+zn23I = canvas.create_polygon(285, 303, 325, 274, 350, 350, outline='black', fill='white', tag='z2_3I', )
+zn3I = canvas.create_polygon(325, 274, 375, 274, 350, 350, outline='black', fill='white', tag='z3I')
+zn34I = canvas.create_polygon(375, 274, 415, 303, 350, 350, outline='black', fill='white', tag='z3_4I')
+zn4I = canvas.create_polygon(430, 350, 415, 303, 350, 350, outline='black', fill='white', tag='z4I')
+zn45I = canvas.create_polygon(430, 350, 415, 397, 350, 350, outline='black', fill='white', tag='z4_5I')
+zn5I = canvas.create_polygon(415, 397, 375, 426, 350, 350, outline='black', fill='white', tag='z5I')
+zn51I = canvas.create_polygon(325, 426, 375, 426, 350, 350, outline='black', fill='white', tag='z5_1I')
+
+# Zone labels
+zn1_label = Label(canvas, text='Zone 1', fg='black', bg='white')
+zn12_label = Label(canvas, text='Zone 1 & 2', fg='black', bg='white')
+zn2_label = Label(canvas, text='Zone 2', fg='black', bg='white')
+zn23_label = Label(canvas, text='Zone 2 & 3', fg='black', bg='white')
+zn3_label = Label(canvas, text='Zone 3', fg='black', bg='white')
+zn34_label = Label(canvas, text='Zone 3 & 4', fg='black', bg='white')
+zn4_label = Label(canvas, text='Zone 4', fg='black', bg='white')
+zn45_label = Label(canvas, text='Zone 4 & 5', fg='black', bg='white')
+zn5_label = Label(canvas, text='Zone 5', fg='black', bg='white')
+zn51_label = Label(canvas, text='Zone 5 & 1', fg='black', bg='white')
+
+canvas.create_window(200, 400, window=zn1_label)
+canvas.create_window(200, 310, window=zn12_label)
+canvas.create_window(250, 225, window=zn2_label)
+canvas.create_window(350, 200, window=zn23_label)
+canvas.create_window(450, 225, window=zn3_label)
+canvas.create_window(500, 310, window=zn34_label)
+canvas.create_window(500, 400, window=zn4_label)
+canvas.create_window(450, 475, window=zn45_label)
+canvas.create_window(350, 500, window=zn5_label)
+canvas.create_window(250, 475, window=zn51_label)
 
 L = ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5',
      '5', '5', '1_5', '1_5', '5', '5', '5', '5', '5', '5', '5', '5', '1', '1', '1', '1', '1', '1', '1_5', '5', '1',
@@ -155,9 +216,8 @@ L = ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', '5', '5', '5', 
      '4_5', '4', '4', '4', '4', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5',
      '5', '5', '1']
 
-# Login Screen
+# Login Screen Config
 top = Toplevel()
-
 top.geometry('300x360')
 top.title('User Login')
 top.configure(background='white')
